1169c459f225c3efd570a169649bbd4ad63f1b12
Kevin Olentino <32975170+KevinOlentino@users.noreply.github.com>
2021-10-01 16:23:59 -0400

    Create ResultadoController.java

---
+++ src/main/java/com/sistema/aposta/controller/ResultadoController.java
@@ -0,0 +1,71 @@
+package com.sistema.aposta.controller;
+
+import com.sistema.aposta.Mensagem;;
+import com.sistema.aposta.biz.ResultadoBiz;
+import com.sistema.aposta.entities.Resultado;
+import com.sistema.aposta.repositories.JogoRepository;
+import com.sistema.aposta.repositories.ResultadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+
+public class ResultadoController {
+
+    @Autowired
+    private ResultadoRepository resultadoRepository;
+
+    @Autowired
+    private JogoRepository jogoRepository;
+
+    @GetMapping
+    @RequestMapping("listar")
+    public List<Resultado> listar() {
+        return resultadoRepository.findAll();
+    }
+
+
+    @PostMapping
+    @RequestMapping("incluir")
+    public Mensagem incluir(@RequestBody Resultado resultado) {
+        resultado.setIdResultado(0);
+        return add(resultado);
+    }
+
+    @PostMapping
+    @RequestMapping("alterar")
+    public Mensagem alterar(@RequestBody Resultado resultado) {
+        return add(resultado);
+    }
+
+    @GetMapping
+    @RequestMapping("/{id}")
+    public Resultado Consultar(@PathVariable int id){
+        return resultadoRepository.findById(id).get();
+    }
+
+
+    public Mensagem add(Resultado resultado) {
+        ResultadoBiz resultadoBiz = new ResultadoBiz(jogoRepository);
+        try
+        {
+            if (resultadoBiz.Validade(resultado)) {
+                this.resultadoRepository.save(resultado);
+                this.resultadoRepository.flush();
+            } else {
+                return resultadoBiz.msg;
+            }
+        }
+        catch (ConstraintViolationException e) {
+            e.getConstraintViolations().forEach(v -> resultadoBiz.msg.mensagens.add(v.getMessage()));
+            return resultadoBiz.msg;
+        }
+
+        resultadoBiz.msg.mensagens.add("OK");
+
+        return resultadoBiz.msg;
+
+
+    }
+}
