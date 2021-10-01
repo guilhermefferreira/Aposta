595c791d3ea96cebd00f5d7145b638cc5e994daf
Guilherme F. Ferreira <gferreira@itelinfo.com>
2021-10-01 15:21:46 -0400

    Clube Entity

--- src/main/java/com/sistema/aposta/entities/Clube.java
+++ src/main/java/com/sistema/aposta/entities/Clube.java
@@ -1,5 +1,64 @@
 package com.sistema.aposta.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Table(name="Clube")
 public class Clube {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idclube")
+	private int idClube;
+	
+	@Column(name = "nome", nullable = false, length = 50)
+    @Pattern(regexp = "[A-z ]{5,50}")
+    private String nome;
+	
+	@Column(name = "telefone",nullable = false, length = 50)
+	private String telefone;
+	
+	@Column(name = "email", nullable = false, length = 50)
+	@Pattern(regexp="(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])", message="O Email é inválido!")
+	private String email;
+
+	public int getIdClube() {
+		return idClube;
+	}
+
+	public void setIdClube(int idClube) {
+		this.idClube = idClube;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 }
