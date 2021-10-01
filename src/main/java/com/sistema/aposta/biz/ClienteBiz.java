dad8b85c44d6333ab20e1ddfd0bb7a75fb00e47c
Marciel S. Soares <msoares@itelinfo.com>
2021-10-01 16:16:18 -0400

    Criação da Biz Cliente

--- src/main/java/com/sistema/aposta/biz/ClienteBiz.java
+++ src/main/java/com/sistema/aposta/biz/ClienteBiz.java
@@ -1,5 +1,33 @@
 package com.sistema.aposta.biz;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sistema.aposta.Mensagem;
+import com.sistema.aposta.entities.Cliente;
+import com.sistema.aposta.repositories.ClienteRepository;
+
 public class ClienteBiz {
+	
+public Mensagem msg;
+	
+	@Autowired
+	public ClienteRepository clienteRepositorio;
+	
+		public Boolean Validade(Cliente cliente) {
+	
+		Boolean result = true;
+		if (cliente.getNome().isEmpty()) {
+			msg.mensagens.add("O nome do cliente não pode ser vazio");
+			result = false;
+		}
+		
+		if (cliente.getEmail().isEmpty()) {
+			msg.mensagens.add("O email do cliente não pode ser vazio");
+			result = false;
+		}
+		
+		return result;
+		
+	}
 
 }
