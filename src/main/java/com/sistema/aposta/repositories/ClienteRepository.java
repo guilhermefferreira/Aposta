c6b92d1f9160a1801d75a696a3187ab018c2b48b
Marciel S. Soares <msoares@itelinfo.com>
2021-10-01 15:43:07 -0400

    Criação da Repositorio Cliente

---
+++ src/main/java/com/sistema/aposta/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.sistema.aposta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sistema.aposta.entities.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
